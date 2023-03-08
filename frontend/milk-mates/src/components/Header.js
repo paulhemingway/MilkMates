4942bd5ac76e2e0b35a2cbaa3123862227c7abfe
Paul Hemingway <60056589+paulhemingway@users.noreply.github.com>
2023-03-08 17:54:02 -0600

    Responsiveness
    Made the nav bar responsive. Still have to put the MIlkMates logo in the menu when on desktop orientation though

--- frontend/milk-mates/src/components/Header.js
+++ frontend/milk-mates/src/components/Header.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React from "react";
 import MilkMatesLogo from "assets/images/logo/logo-pink.png";
 import { Link } from "react-router-dom";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 
-import "../assets/styles/Header.scss"
+import "../assets/styles/Header.scss";
 
-export default function Header() {
+export default function Header(props) {
+  const isAtLanding = window.location.pathname === "/";
+
+  function NavButton() {
+    const Icon = props.collapsed ? IoMdMenu : IoMdClose;
+
+    return <Icon onClick={toggleCollapse} onKeyDown={(e) => {
+      if (e.keyCode === 32) {
+        toggleCollapse();
+      }
+    }} tabIndex="0" />
+  }
+
+  const toggleCollapse = () => {
+    props.toggleCollapse()
+
+  }
+
   return (
     <div className="top-bar">
-        <div className="logo">
-          <div className="logo-circle"></div>
-          <img src={MilkMatesLogo} alt="MilkMates logo" />
-        </div>
+      <div className="logo">
+        <div className="logo-circle"></div>
+        <img src={MilkMatesLogo} alt="MilkMates logo" />
+      </div>
 
-        <h1>MilkMates</h1>
-        <div className="help-container">
+      <h1>MilkMates</h1>
+      <div className="right">
+        {isAtLanding ? (
           <Link to="/help" className="link">
             Need Help?
           </Link>
-        </div>
+        ) : (
+          <NavButton onClick={toggleCollapse} />
+        )}
       </div>
-  )
+    </div>
+  );
 }
