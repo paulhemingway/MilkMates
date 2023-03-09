2ce2da3ad000873c1fdeae1003822584f802bc7b
Paul Hemingway <60056589+paulhemingway@users.noreply.github.com>
2023-03-09 16:55:37 -0600

    Responsiveness
    made it responsive

--- frontend/milk-mates/src/components/Header.js
+++ frontend/milk-mates/src/components/Header.js
@@ -1,45 +1,23 @@
-import React from "react";
+import React from 'react'
 import MilkMatesLogo from "assets/images/logo/logo-pink.png";
 import { Link } from "react-router-dom";
-import { IoMdMenu, IoMdClose } from "react-icons/io";
 
-import "../assets/styles/Header.scss";
+import "../assets/styles/Header.scss"
 
-export default function Header(props) {
-  const isAtLanding = window.location.pathname === "/";
-
-  function NavButton() {
-    const Icon = props.collapsed ? IoMdMenu : IoMdClose;
-
-    return <Icon onClick={toggleCollapse} onKeyDown={(e) => {
-      if (e.keyCode === 32) {
-        toggleCollapse();
-      }
-    }} tabIndex="0" />
-  }
-
-  const toggleCollapse = () => {
-    props.toggleCollapse()
-
-  }
-
+export default function Header() {
   return (
     <div className="top-bar">
-      <div className="logo">
-        <div className="logo-circle"></div>
-        <img src={MilkMatesLogo} alt="MilkMates logo" />
-      </div>
+        <div className="logo">
+          <div className="logo-circle"></div>
+          <img src={MilkMatesLogo} alt="MilkMates logo" />
+        </div>
 
-      <h1>MilkMates</h1>
-      <div className="right">
-        {isAtLanding ? (
+        <h1>MilkMates</h1>
+        <div className="help-container">
           <Link to="/help" className="link">
             Need Help?
           </Link>
-        ) : (
-          <NavButton onClick={toggleCollapse} />
-        )}
+        </div>
       </div>
-    </div>
-  );
+  )
 }
